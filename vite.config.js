9e725e614f2d02b7da18d8009d6667b0c6d7fecb
RICHMOND APPIAH-KUBI <richmond.appiah-kubi@cbi-africa.local>
2026-07-09 13:33:28 +0000

    chore: split vendor chunks to resolve size warning limit

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
 })
